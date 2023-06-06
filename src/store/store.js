e207aa89dea1636f27a3990a1b83b55cd99124f1
Саламат Кадыров <salamat@MacBook-Air-Salamat.local>
2023-06-06 11:03:08 +0600

    auth redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authReducer } from "./auth/authSlice";
 import { productsReducer } from "./products/productsSlice";
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
+    auth: authReducer,
   },
 });
