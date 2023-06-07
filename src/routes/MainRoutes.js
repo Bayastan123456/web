fb28948bdc326614847d2bc065b44eddbdfde1d3
Dzhumar <sharipojumar@gmail.com>
2023-06-07 11:39:38 +0600

    newcommit

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -4,14 +4,20 @@ import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import RegisterPage from "../pages/RegisterPage";
+import ProductsCard from "../components/Products/ProductsCard";
+import ProductsList from "../components/Products/ProductsList";
+import ProductsAccardion from "../components/Products/ProductsAccardion";
+import ProductsPage from "../pages/ProductsPage";
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
     { link: "/", element: <HomePage />, id: 1 },
     { link: "/login", element: <LoginPage />, id: 2 },
     { link: "/register", element: <RegisterPage />, id: 3 },
-    { link: "/", element: <HomePage />, id: 4 },
-    { link: "*", element: <NotFoundPage />, id: 5 },
+    { link: "/productsList", element: <ProductsList />, id: 4 },
+    { link: "/productsAccordion", element: <ProductsAccardion />, id: 5 },
+    { link: "/productsPage", element: <ProductsPage />, id: 5 },
+    { link: "*", element: <NotFoundPage />, id: 6 },
   ];
   return (
     <>
