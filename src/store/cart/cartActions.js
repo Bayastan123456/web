ce3b3b03adc0d14bc4dd0e490009a08ab87aacbe
aidarbek312 <aidarbek312@gmail.com>
2023-06-09 15:23:10 +0600

    newaaaaaa

--- src/store/cart/cartActions.js
+++ src/store/cart/cartActions.js
@@ -10,8 +10,5 @@ export const createCart = createAction("@cart/createCart", () => {
     );
     cart = { products: [], totalPrice: 0 };
   }
-<<<<<<< HEAD
-=======
   return cart;
->>>>>>> 1e5913f81d578e6088a04f5d9f00a15e7ad39d1c
 });
